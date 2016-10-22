8bcedf533ba457324a087424876aae7af4eedfc7
Felipe Moraes <felipemoraes@dcc.ufmg.br>
2016-10-22 19:22:55 -0200

    Restructed folds

---
+++ src/indexer/br/ufmg/dcc/latin/indexer/LuceneIndexing.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.ufmg.dcc.latin.indexer;
+
+/**
+ * @author felipemoraes
+ *
+ */
+public class LuceneIndexing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
