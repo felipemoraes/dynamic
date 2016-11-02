9f774f81305bd8ebc488cf1eec4c9f7af6006e66
Felipe Moraes <fe.moraesg@gmail.com>
2016-11-02 15:52:18 -0200

    Making space for unit testing

---
+++ core/src/test/java/br/ufmg/dcc/latin/scoring/diversity/DiversityScoringTest.java
@@ -0,0 +1,34 @@
+package br.ufmg.dcc.latin.scoring.diversity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DiversityScoringTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
