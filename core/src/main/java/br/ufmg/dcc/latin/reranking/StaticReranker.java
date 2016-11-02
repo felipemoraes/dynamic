4bf2fc3e4a8c131c48f4fc577427fdbe6291ff9e
Felipe Moraes <fe.moraesg@gmail.com>
2016-11-02 15:49:20 -0200

    Defining more interfaces and scope

---
+++ core/src/main/java/br/ufmg/dcc/latin/reranking/StaticReranker.java
@@ -0,0 +1,5 @@
+package br.ufmg.dcc.latin.reranking;
+
+public class StaticReranker implements Reranker {
+
+}
---
+++ core/src/main/java/br/ufmg/dcc/latin/scoring/diversity/xQuAD.java
@@ -0,0 +1,5 @@
+package br.ufmg.dcc.latin.scoring.diversity;
+
+public class xQuAD {
+
+}
