7b0033c4160fa9638c5583475d6dd941049219d0
Felipe Moraes <fe.moraesg@gmail.com>
2016-12-20 13:55:11 -0200

    Fix issue with WeightedPassageRetrieval

--- core/src/main/java/br/ufmg/dcc/latin/learning/Static.java
+++ core/src/main/java/br/ufmg/dcc/latin/learning/Static.java
@@ -37,7 +37,7 @@ public class Static implements Learner {
 	
 	@Override
 	public void setupReranker(String rerankerName) {
-		reranker = InteractiveRerankerFactory.getInstance(rerankerName, "WeightedPassageAspectMining");
+		reranker = InteractiveRerankerFactory.getInstance(rerankerName, "PassageAspectMining");
 	}
 
 	@Override
