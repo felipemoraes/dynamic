0ad25bcd30cde057d5c7f82722a306fa1f3efdc2
Felipe Moraes <fe.moraesg@gmail.com>
2016-12-30 12:10:08 -0200

    SimulatedNonRelevant at second iteration

--- core/src/main/java/br/ufmg/dcc/latin/baseline/BaselineRanker.java
+++ core/src/main/java/br/ufmg/dcc/latin/baseline/BaselineRanker.java
@@ -210,10 +210,11 @@ public class BaselineRanker {
 		TreeList<Integer> irrelevants = new TreeList<Integer>();
 		
 		for (int i = 0; i < relevances.length; i++) {
-			if (i >= 50) {
-				stays[i] = true;
-				continue;
-			}
+			//if (i >= 50) {
+			//	stays[i] = true;
+			//	continue;
+			//}
+			
 			if (relevances[i] == 0 ){
 				countIrr++;
 				irrelevants.add(i);
