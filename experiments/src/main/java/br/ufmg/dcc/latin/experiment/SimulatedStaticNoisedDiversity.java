2d60fdbcbac73a5fd8422767f68896a9f943e8b9
Felipe Moraes <fe.moraesg@gmail.com>
2017-01-07 12:11:16 -0200

    Remove xMMR from experiments

--- experiments/src/main/java/br/ufmg/dcc/latin/experiment/SimulatedStaticNoisedDiversity.java
+++ experiments/src/main/java/br/ufmg/dcc/latin/experiment/SimulatedStaticNoisedDiversity.java
@@ -68,7 +68,7 @@ public class SimulatedStaticNoisedDiversity {
 			int count = 0;
 		    for (int k = 0; k < 100; k++) {
 		    	
-			   double epsilon = 3;
+			   double epsilon = 5;
 					
 			   double kl = trecUser.generateSubtopicsWithNoise(epsilon, baselineResultSet.docnos);
 			   
