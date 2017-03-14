4910f188719ffb107c66956e751be1bbd7be1446
Felipe Moraes <fe.moraesg@gmail.com>
2017-03-14 19:08:45 -0300

    Implemented HPM2

--- core/src/main/java/br/ufmg/dcc/latin/learning/Static.java
+++ core/src/main/java/br/ufmg/dcc/latin/learning/Static.java
@@ -36,7 +36,7 @@ public class Static implements Learner {
 	
 	@Override
 	public void setupReranker(String rerankerName) {
-		reranker = InteractiveRerankerFactory.getInstance(rerankerName, "FeaturedAspectMining");
+		reranker = InteractiveRerankerFactory.getInstance(rerankerName, "PassageAspectMining");
 	}
 
 	@Override
