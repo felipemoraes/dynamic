2becb1cf736b995395f1e3171887b6d43d3b556c
Felipe Moraes <fe.moraesg@gmail.com>
2017-04-26 20:50:38 -0300

    back

--- core/src/main/java/br/ufmg/dcc/latin/baseline/BaselineRanker.java
+++ core/src/main/java/br/ufmg/dcc/latin/baseline/BaselineRanker.java
@@ -76,6 +76,7 @@ public class BaselineRanker {
 			sortedScores[i] = currentResultSet.scores[best];
 			selected.put(best);
 			
+			
 		}
 		currentResultSet.scores = sortedScores;
 		currentResultSet.docnos = sortedDocNos;
